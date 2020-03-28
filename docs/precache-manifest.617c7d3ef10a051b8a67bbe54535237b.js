f8874cfd5086976a010210dafd9ff1d0d7ad196a
leo <lpylpyleo@gmail.com>
2020-03-29 06:33:22 +0800

    init

---
+++ docs/precache-manifest.617c7d3ef10a051b8a67bbe54535237b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f27050151acfb5847dee",
+    "url": "/vue-calculator/css/app.d67a22d4.css"
+  },
+  {
+    "revision": "adc413a055e19d896847742f856e453c",
+    "url": "/vue-calculator/index.html"
+  },
+  {
+    "revision": "bad96b0414070c2f42e3",
+    "url": "/vue-calculator/js/about.d4ace205.js"
+  },
+  {
+    "revision": "f27050151acfb5847dee",
+    "url": "/vue-calculator/js/app.92c70080.js"
+  },
+  {
+    "revision": "fbf639e1deb7df77a467",
+    "url": "/vue-calculator/js/chunk-vendors.83bea35d.js"
+  },
+  {
+    "revision": "c795750ae58a2a4ac4a545139aeb1ab9",
+    "url": "/vue-calculator/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-calculator/robots.txt"
+  }
+]);
